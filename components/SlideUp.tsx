f4d9492827dd56f5239e50db268d3c4ab237249e
Anthony Lopez <Alopez3030@gmail.com>
2023-11-17 17:30:27 -0500

    trial

--- components/SlideUp.tsx
+++ components/SlideUp.tsx
@@ -1,36 +1,44 @@
 "use client" // this is a client component
+import React, { useEffect, useRef, ReactNode } from "react";
 
-import React, { useEffect, useRef, ReactNode } from "react"
 interface Props {
-  offset?: string
-  children?: ReactNode
+  offset?: string;
+  children?: ReactNode;
   // any props that come into the component
 }
 
 export default function SlideUp({ children, offset = "0px" }: Props) {
-  const ref = useRef(null)
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            entry.target.classList.remove("opacity-0")
-            entry.target.classList.add("animate-slideUpCubiBezier")
+            entry.target.classList.remove("opacity-0");
+            entry.target.classList.add("animate-slideUpCubiBezier");
           }
-        })
+        });
       },
       { rootMargin: offset }
-    )
+    );
 
-    if (ref.current) {
-      observer.observe(ref.current)
+    const currentRef = ref.current; // Store ref.current in a variable
+
+    if (currentRef) {
+      observer.observe(currentRef);
     }
-  }, [ref])
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [offset]); // Include 'offset' in the dependency array
 
   return (
     <div ref={ref} className="relative opacity-0">
       {children}
     </div>
-  )
+  );
 }
